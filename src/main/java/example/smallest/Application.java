38b5d82e5c252939b9661c6db730d819246ee970
KoppulaRajender <koppularajender18@gmail.com>
2021-07-23 08:16:08 +0530

    adding vul

--- src/main/java/example/smallest/Application.java
+++ src/main/java/example/smallest/Application.java
@@ -11,5 +11,7 @@ public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
     	 SpringApplication.run(Application.class, args);
+         int a = 10;
+         String test = "10.10.10.90";
     }
 }
